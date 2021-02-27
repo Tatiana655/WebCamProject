7176277664d866178fd003a9d31e3df25c2fdbc8
Tatiana655 <61006466+Tatiana655@users.noreply.github.com>
2021-02-27 18:10:41 +0300

    add scrolls

--- Mask.py
+++ Mask.py
@@ -1,48 +1,103 @@
 import cv2
 import numpy as np
-size = 20
+
+# обработка изображения с маркером, современная обработка изображений
+# ик камера
+# обмен симвоолами, первый спринт + документация алгоритмов. как выбирать алг, фильтры
+# linka, ,kfujhjlyfz wtkm ? или развлекалово
+# user requirement
+
+size = 20  # ребро квадрата-считывателя
+# асположение квадрата
 X = 200
 Y = 100
+# режим работы калибровка/рисование (мб и больше будет)
 mode = False
-delt = 0
+# погрешность
+eps = 0
 
+
+# вычисляет min/max комполенты двых цветов-векторов
 def find_min_coomp(vec1, vec2):
     for i in range(len(vec1)):
         vec1[i] = min(vec1[i], vec2[i])
     return vec1
+
+
 def find_max_coomp(vec1, vec2):
     for i in range(len(vec1)):
         vec1[i] = max(vec1[i], vec2[i])
     return vec1
-def subtract_vectors(v, w):
-    res =3*255
-    for i in range(len(v)):
-        res = min(res, int(v[i])-int(w[i]))
-    return res
 
-#ищет два вектора цвета в квадратике(это буквально квадрат)
+
+# ищет два вектора цвета на маркере
 def find_all_colors(img):
     min_color = [255, 255, 255]
     max_color = [0, 0, 0]
-    # мне кажется, тут можно написать и короче. Я не знаток питона :(. тогда мб функция не нужна
+    # мне кажется, тут можно написать и короче
     for i in range(Y, Y + size):
         for j in range(X, X + size):
             for k in range(3):
                 min_color[k] = min(img[i][j][k], min_color[k])
-                if min_color[k]>=delt:
-                    min_color[k] -= delt
+                if min_color[k] >= eps:
+                    min_color[k] -= eps
                 max_color[k] = max(img[i][j][k], min_color[k])
-                if max_color[k] <=255-delt:
-                    max_color[k]+=delt;
+                if max_color[k] <= 255 - eps:
+                    max_color[k] += eps
     return min_color, max_color
 
 
+def Scrolls(cap, min_color, max_color):
+    cv2.namedWindow("result")  # создаем главное окно
+    cv2.namedWindow("settings")  # создаем окно настроек
+
+    # создаем 6 бегунков для настройки начального и конечного цвета фильтра
+    cv2.createTrackbar('r_min', 'settings', min_color[0], 255, lambda x:x)
+    cv2.createTrackbar('g_min', 'settings', min_color[1], 255, lambda x:x)
+    cv2.createTrackbar('b_min', 'settings', min_color[2], 255, lambda x:x)
+    cv2.createTrackbar('r_max', 'settings', max_color[0], 255, lambda x:x)
+    cv2.createTrackbar('g_max', 'settings', max_color[1], 255, lambda x:x)
+    cv2.createTrackbar('b_max', 'settings', max_color[2], 255, lambda x:x)
+
+    while True:
+        flag, imgtmp = cap.read()
+        imgtmp = cv2.flip(imgtmp, 1)
+        trash = np.zeros_like(imgtmp)
+        # считываем значения бегунков
+        h1 = cv2.getTrackbarPos('r_min', 'settings')
+        s1 = cv2.getTrackbarPos('g_min', 'settings')
+        v1 = cv2.getTrackbarPos('b_min', 'settings')
+        h2 = cv2.getTrackbarPos('r_max', 'settings')
+        s2 = cv2.getTrackbarPos('g_max', 'settings')
+        v2 = cv2.getTrackbarPos('b_max', 'settings')
+
+        # формируем начальный и конечный цвет фильтра
+        h_min = np.array((h1, s1, v1))
+        h_max = np.array((h2, s2, v2))
+
+        # накладываем фильтр на кадр
+        filter = cv2.inRange(imgtmp, h_min, h_max)
+        filter = cv2.medianBlur(filter, 15)
+        trash = cv2.add(trash, cv2.bitwise_and(imgtmp, imgtmp, mask=filter))
+
+        cv2.imshow('result', np.hstack([imgtmp, trash]))
+
+        ch = cv2.waitKey(5)
+        if ch == 27:# wait for ESC key to exit|| хорошо бы "OK" найти какой-нибудь
+            cv2.destroyWindow("result")
+            cv2.destroyWindow("settings")
+            return [h1, s1, v1], [h2, s2, v2]
+
+
+
+# это хорошо бы в мейн
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-x_vec = []
-y_vec = []
+# x_vec = []
+# y_vec = []
 min_color = [255, 255, 255]
 max_color = [0, 0, 0]
-f = 0
+count_col = 0
+
 ret, img = cap.read()
 Marker = np.zeros_like(img)
 Point = np.zeros_like(img)
@@ -50,47 +105,43 @@ Point = np.zeros_like(img)
 mask = np.zeros_like(img)
 mask = cv2.flip(mask, 1)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(10,10))
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
 fgbg = cv2.createBackgroundSubtractorKNN(50, 200.0, False)
 
 while True:
     ret, img = cap.read()
     img = cv2.flip(img, 1)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    start_point = (X-1, Y-1)
-    end_point = (X + size+1, Y + size+1)
+
+    start_point = (X - 1, Y - 1)
+    end_point = (X + size + 1, Y + size + 1)
     color = (255, 0, 0)
     thickness = 1
     Marker = np.zeros_like(img)
-    if mode == False:
+    # режим калибровки
+    if not mode:
         img = cv2.rectangle(img, start_point, end_point, color, thickness)
         cv2.imshow("WM", img)
 
-    # here something like: "place the item(?) in the circle and qlick "q""//To define a marker//что-то типа калибровки, но как это в КЗ называется - пас
-    if cv2.waitKey(10) == ord('a'):#на самом деле я просто других укв не знаю//посмотреть другие буквы//возможно стоит калибровать на нескольких кадрах, там даже так погрешность большая что-то
-        #тут надо найти максимальную разность цветов//вернуть 2 RGB
+    if cv2.waitKey(10) == ord('a'):
+        # найти максимальную разность цветов//вернуть 2 RGB//запихнуть в одну функцию-файл
         min_color1, max_color1 = find_all_colors(img)
-        min_color = find_min_coomp(min_color,min_color1)
-        max_color = find_max_coomp(max_color,max_color1)
-        f+=1
-        if f == 4:
+        min_color = find_min_coomp(min_color, min_color1)
+        max_color = find_max_coomp(max_color, max_color1)
+        count_col += 1  # количество щёлчков
+        if count_col == 10:
             mode = True
-    if (mode):
-        #поиск в диапазоне цветов
-        L1 = np.array(min_color)
-        U1 = np.array(max_color)
+            # бегунки после калибровки для проверки тцут
+            min_color, max_color = Scrolls(cap, min_color, max_color)
 
-        hsv_min = np.array(min_color, np.uint8)
-        hsv_max = np.array(max_color, np.uint8)
+    # режим рисования
+    if mode:
+        # поиск в диапазоне цветов, тут можно сократить код
 
-        thresh = cv2.inRange(hsv, hsv_min, hsv_max)
-        m1 = cv2.inRange(img, L1, U1)
+        m1 = cv2.inRange(img, np.array(min_color), np.array(max_color))
         m1 = cv2.medianBlur(m1, 15)
-        #user reqaierment
+
         Marker = cv2.add(Marker, cv2.bitwise_and(img, img, mask=m1))
 
-        #hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        #gray_image = cv2.cvtColor(m1, cv2.COLOR_BGR2GRAY)
         moments = cv2.moments(m1, 1)
         dM01 = moments['m01']
         dM10 = moments['m10']
@@ -101,18 +152,15 @@ while True:
             y = int(dM01 / dArea)
             mask = cv2.circle(mask, (x, y), 5, (50, 100, 255), -1)
             Point = cv2.add(Point, mask)
-        #обработка изображения с маркером, современная обработка изображений
-        #ик камера
-        #обмен симвоолами, первый спринт + документация алгоритмов. как выбирать алг, фильтры
-        #linka, ,kfujhjlyfz wtkm ? или развлекалово
+
         fgmask = fgbg.apply(Marker)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
         cv2.imshow("WM", np.hstack([img, Marker, Point]))
-    #cv2.imshow("Cam", img)
-    if cv2.waitKey(10) == 27: # Клавиша Esc
+
+    if cv2.waitKey(10) == 27:  # Клавиша Esc
         break
-    if not cv2.getWindowProperty('WM', 0) >= 0: #крестик
+    if not cv2.getWindowProperty('WM', 0) >= 0:  # крестик
         break
 
 cap.release()
